b9dfe8d98eb9eface8870d8bafbb14bee44a0f08
Mia Larsen <54668407+miadp88@users.noreply.github.com>
2023-12-11 13:34:22 +0100

    setting up navbar

---
+++ src/components/Layout/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <fwb-navbar class="bg-black">
+      <template #logo>
+        <fwb-navbar-logo alt="Arkens Logo" image-url="/src/assets/images/13694_logo.jpg" link="../../views/WelcomeView.vue"></fwb-navbar-logo>
+      </template>
+      <template #default="{isShowMenu}">
+      </template>
+      <template #right-side>
+        <fwb-button color="light">
+          Log ind
+        </fwb-button>
+      </template>
+    </fwb-navbar>
+  </template>
+  
+  <script setup>
+  import {
+    FwbButton,
+    FwbNavbar,
+    FwbNavbarLogo,
+  } from 'flowbite-vue'
+  </script>
+
+<style>
+  .NavBar{
+    background-color: black!important;
+  }
+</style>
