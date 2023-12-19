5c0091cee1c3368e9e08964cff47f3246ddcb845
Mia Larsen <54668407+miadp88@users.noreply.github.com>
2023-12-19 14:24:16 +0100

    styling the notification(succes add to cart) so that it is blue and shown i the center of the screenview

--- src/components/Layout/Notification.vue
+++ src/components/Layout/Notification.vue
@@ -1,47 +1,77 @@
-<!-- src/components/Notification.vue -->
+<!-- src/components/Layout/Notification.vue -->
 <template>
-    <div v-if="show" class="notification">
-      <p>{{ message }}</p>
-      <button @click="closeNotification">Close</button>
-    </div>
-  </template>
-  
-  <script>
-  import { ref } from 'vue';
-  
-  export default {
-    props: {
-      message: String,
-    },
-    setup(props) {
-      const show = ref(true);
-  
-      const closeNotification = () => {
-        show.value = false;
-      };
-  
-      return {
-        show,
-        closeNotification,
-      };
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .notification {
-    position: fixed;
-    top: 16px;
-    right: 16px;
-    background-color: #4caf50;
-    color: white;
-    padding: 16px;
-    border-radius: 4px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 300px;
-  }
-  </style>
-  
+  <div v-if="show" class="notification">
+    <svg
+      class="checkmark-icon"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#4CAF50"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+      <polyline points="22 4 12 14.01 9 11.01"></polyline>
+    </svg>
+    <p>{{ message }}</p>
+    <button @click="closeNotification">Close</button>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+  props: {
+    message: String,
+  },
+  setup(props) {
+    const show = ref(true);
+
+    const closeNotification = () => {
+      show.value = false;
+    };
+
+    // Watch for changes in the message and show the notification
+    watch(props.message, (newValue, oldValue) => {
+      if (newValue !== oldValue) {
+        show.value = true;
+        setTimeout(() => {
+          show.value = false;
+        }, 3000); // Adjust the timeout as needed
+      }
+    });
+
+    return {
+      show,
+      closeNotification,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.notification {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(122, 228, 245, 0.98); /* Adjust opacity */
+  padding: 30px; /* Adjust padding to increase size */
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.checkmark-icon {
+  width: 24px;
+  height: 24px;
+  color: #4CAF50;
+}
+
+.notification p {
+  margin: 0 10px;
+}
+</style>
