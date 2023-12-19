99e212c494456ce534c7ec51e7f34fa35bc584b4
Mia Larsen <54668407+miadp88@users.noreply.github.com>
2023-12-19 10:14:13 +0100

    adding SVG cart icon

--- src/components/Layout/NavBarOrderComponent.vue
+++ src/components/Layout/NavBarOrderComponent.vue
@@ -6,7 +6,7 @@
 
     <div class="relative" @click="showCart">
       <svg
-        class="w-8 h-8 cursor-pointer"
+        class="w-8 h-8 cursor-pointer text-white"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
@@ -16,9 +16,11 @@
           stroke-linecap="round"
           stroke-linejoin="round"
           stroke-width="2"
-          d="M9 19c-1.816 0-3-1.184-3-3s1.184-3 3-3 3 1.184 3 3-1.184 3-3 3zm13-5V7a3 3 0 0 0-3-3H4a3 3 0 0 0-3 3v7h2a3 3 0 0 0 6 0h4a3 3 0 0 0 6 0h2zM9 16v2a1 1 0 1 0 2 0v-2m6 0v2a1 1 0 1 0 2 0v-2"
-        ></path>
+          d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"></path>
       </svg>
+
+
+
 
       <div class="absolute top-0 right-0 -mt-1 -mr-1">
         <div class="bg-red-500 text-white rounded-full w-4 h-4 text-xs flex items-center justify-center">
