56a20db82d02896fb32b284b133a1e81409424ca
Mia Larsen <54668407+miadp88@users.noreply.github.com>
2023-12-19 21:07:13 +0100

    styling the MenuAddView rge same ass MenuView

--- src/views/MenuAddView.vue
+++ src/views/MenuAddView.vue
@@ -9,12 +9,23 @@
 
       <!-- Section for Side Order Cards -->
       <section>
-        <h1 class="head1 mt-20 text-5xl font-extrabold tracking-tight text-center leading-none text-white md:text-5xl lg:text-6xl dark:text-white tracking-wider">
+        <h1 class="mt-20 text-5xl font-extrabold tracking-tight text-center leading-none text-white md:text-5xl lg:text-6xl dark:text-white tracking-wider">
           Vælg Side Orders
         </h1>
-        <div :class="'head2 flex items-center justify-center min-h-screen px-6'">
+        <div class="CardWrapper flex flex-wrap mt-10 justify-center min-h-fit px-6">
           <!-- Use Card component for side orders -->
           <Card v-for="sideOrder in getSideOrders()" :key="sideOrder.id" :menu="sideOrder" :imagePath="sideOrder.image" />
+        </div>
+        <div class="btnNext m-20 flex items-center justify-center">
+          <router-link to="/userInfoView" class="p-4 bg-transparent text-center text-white rounded-full hover:text-btnBlue flex items-center">
+            <span>Next</span>
+            <!-- Add an SVG icon to the right -->
+            <span class="ml-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 w-5">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"/>
+              </svg>
+            </span>
+          </router-link>
         </div>
       </section>
     </main>
@@ -39,10 +50,25 @@ const sideOrders = ref([
 
   {
     id: 2,
-    name: "Side Order 1",
+    name: "Side Order 2",
     price: 50,
     image: "../../src/assets/images/Menu5.jpg",
+  }, 
+  {
+    id: 3,
+    name: "Side Order 3",
+    price: 50,
+    image: "../../src/assets/images/Menu1.jpeg",
   },
+  {
+    id: 4,
+    name: "Side Order 4",
+    price: 50,
+    image: "../../src/assets/images/Menu2.jpeg",
+  },
+  
+
+  
   // Add more side orders as needed
 ]);
 
@@ -64,22 +90,17 @@ const getSideOrders = () => {
   z-index: -1;
 }
 
-.nav-bar-order {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 1!important; 
-}
-
-.head1, .head2 {
-  margin-top: 10vh !important;
-  font-size: 5em !important;
-}
-
-@media screen and (max-width: 511px) {
-  h1 {
-    font-size: 2em !important;
-  }
+.btnNext {
+  border: solid 3px white;
+  color: white;
+  width: 8em;
+  border-radius: 22em;
+  margin: auto;
+  margin-top: 8vh;
+  margin-bottom: 8vh;
+  font-size: x-large;
+  height: 2em;
+  letter-spacing: 5px;
+  text-align: center;
 }
 </style>
--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -5,7 +5,8 @@
     <main class="main-content">
       <HeroAll />
       <section class="CardSection">
-        <h1 class="mt-20 text-5xl font-extrabold tracking-tight text-center leading-none text-white md:text-5xl lg:text-6xl dark:text-white tracking-wider">Vælg Sæson Menu</h1>
+        <h1 class="mt-20 text-5xl font-extrabold tracking-tight text-center leading-none text-white md:text-5xl lg:text-6xl dark:text-white tracking-wider">
+        Vælg Sæson Menu</h1>
         <!-- Use flex and flex-wrap classes for responsiveness -->
         <div class="CardWrapper flex flex-wrap mt-10 justify-center min-h-fit px-6">
           <Card v-for="menu in getMenu()" :key="menu.id" :menu="menu" :imagePath="menu.image" />
