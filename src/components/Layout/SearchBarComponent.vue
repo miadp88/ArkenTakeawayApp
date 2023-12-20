ddd61fa3f9c94603cc1bfe4bb4f4fedb1ec04ebd
Mia Larsen <54668407+miadp88@users.noreply.github.com>
2023-12-21 00:37:30 +0100

    setting up the serchbar in the headedr of OrderView

--- src/components/Layout/SearchBarComponent.vue
+++ src/components/Layout/SearchBarComponent.vue
@@ -1,1 +1,34 @@
-<template>SearchBar</template>
+<template>
+    <div class="search-bar">
+      <input v-model="searchTerm" type="text" placeholder="Search..." />
+      <button @click="search">Search</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const searchTerm = ref('');
+  
+  const search = () => {
+    // You can implement your search logic here
+    console.log('Searching for:', searchTerm.value);
+  };
+  </script>
+  
+  <style scoped>
+  .search-bar {
+    display: flex;
+    align-items: center;
+  }
+  
+  input {
+    padding: 5px;
+    margin-right: 5px;
+  }
+  
+  button {
+    padding: 5px 10px;
+  }
+  </style>
+  
