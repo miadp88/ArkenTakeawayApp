df00859281d1b13ab48ceafd722d166cf53887a3
Mia Larsen <54668407+miadp88@users.noreply.github.com>
2023-12-20 14:03:57 +0100

    setting up both header and footer to have higher z-index then main

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,49 +1,45 @@
 <template>
-  <div class="menu-view">
-      <header>
+  <div class="app">
+    <header>
       <NavBar />
     </header>
-    <div main-content-weapper>
-      <main class="main-content">
+
+    <div class="content-wrapper">
+      <main>
         <HeroWelcome />
-       
-        <a href="" class="scroll-down-link scroll-down-arrow" data-iconfont="ETmodules" data-icon>
-        </a>
+        <a href="" class="scroll-down-link scroll-down-arrow" data-iconfont="ETmodules" data-icon></a>
       </main>
 
-      <footer class="FooterWelcome">
-        <Footer />
-      </footer>
+      <router-view />
     </div>
-    <router-view />
+
+    <footer class="FooterWelcome">
+      <Footer />
+    </footer>
   </div>
 </template>
 
 <script setup>
-
-/*imports*/
-
+/* imports */
 import NavBar from '@/components/Layout/NavBarComponent.vue'
 import Footer from '@/components/Layout/FooterComponent.vue'
 import HeroWelcome from '@/components/Layout/HeroWelcomeComponent.vue'
-
 </script>
 
 <style scoped>
-.menu-view {
-  position: relative;
+.app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 }
 
-.main-content {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: -1
-
+header {
+  z-index: 1; /* Ensure the header stays on top */
 }
 
-
-
-
+.content-wrapper {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+}
 </style>
