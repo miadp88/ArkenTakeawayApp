0b68d9b520b5b6e30a2de84abb283b7639fd7510
Mia Larsen <54668407+miadp88@users.noreply.github.com>
2023-12-21 00:39:34 +0100

    setting up the datepicker and checkables in the header of the OrderView

--- src/components/Layout/CheckablesComponent.vue
+++ src/components/Layout/CheckablesComponent.vue
@@ -1,1 +1,36 @@
-<template>Checkables</template>
+<template>
+    <div class="checkables">
+      <label>
+        <input type="checkbox" v-model="checkable1" @change="handleChange('Checkable 1')" />
+        Checkable 1
+      </label>
+      <label>
+        <input type="checkbox" v-model="checkable2" @change="handleChange('Checkable 2')" />
+        Checkable 2
+      </label>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const checkable1 = ref(false);
+  const checkable2 = ref(false);
+  
+  const handleChange = (checkableName) => {
+    // You can implement your logic when a checkable is changed
+    console.log(`${checkableName} changed to ${checkableName ? 'checked' : 'unchecked'}`);
+  };
+  </script>
+  
+  <style scoped>
+  .checkables {
+    display: flex;
+    flex-direction: column;
+  }
+  
+  label {
+    margin-bottom: 5px;
+  }
+  </style>
+  
--- src/components/Layout/DatePickerComponent.vue
+++ src/components/Layout/DatePickerComponent.vue
@@ -1,1 +1,35 @@
-<template>DatePicker</template>
+<template>
+    <div class="date-picker">
+      <label for="datepicker">Select Date:</label>
+      <input
+        v-model="selectedDate"
+        type="date"
+        id="datepicker"
+        @change="dateSelected"
+      />
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const selectedDate = ref('');
+  
+  const dateSelected = () => {
+    // You can implement your logic when a date is selected
+    console.log('Selected Date:', selectedDate.value);
+  };
+  </script>
+  
+  <style scoped>
+  .date-picker {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 10px;
+  }
+  
+  label {
+    margin-bottom: 5px;
+  }
+  </style>
+  
